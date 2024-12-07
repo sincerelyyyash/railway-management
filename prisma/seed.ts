d1a5c542ed0e402199c21ee125508cdd974da3fe
sincerelyyyash <yta0088@gmail.com>
2024-12-07 06:19:44 +0530

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,58 +7,43 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 
-const seed = async () => {
-  const adminUser = {
-    name: 'Yash Thakur',
-    email: 'yash@mail.com',
-    password: 'admin123',
-    role: UserRole.ADMIN,
-  };
+async function main() {
   const users = [
     {
+      name: 'Yash Thakur',
+      email: 'yash@gmail.com',
+      password: 'admin123',
+      role: UserRole.ADMIN,
+    },
+    {
       name: 'Aditya Singh',
-      email: 'aditya@example.com',
+      email: 'aditya@mail.com',
       password: 'aditya123',
       role: UserRole.USER,
     },
     {
       name: 'Harman Singh',
-      email: 'harman@example.com',
+      email: 'harman@mail.com',
       password: 'harman123',
       role: UserRole.USER,
     },
   ];
 
-  const hashedAdminPassword = await bcrypt.hash(adminUser.password, 10);
-  const hashedUserPasswords = await Promise.all(
-    users.map(user => bcrypt.hash(user.password, 10))
+  const hashedUsers = await Promise.all(
+    users.map(async (user) => ({
+      name: user.name,
+      email: user.email,
+      password: await bcrypt.hash(user.password, 10),
+      role: user.role,
+    }))
   );
 
-  try {
-    await prisma.user.create({
-      data: {
-        name: adminUser.name,
-        email: adminUser.email,
-        password: hashedAdminPassword,
-        role: adminUser.role,
-      },
-    });
+  await prisma.user.createMany({
+    data: hashedUsers,
+  });
+}
 
-    for (let i = 0; i < users.length; i++) {
-      await prisma.user.create({
-        data: {
-          name: users[i].name,
-          email: users[i].email,
-          password: hashedUserPasswords[i],
-          role: users[i].role,
-        },
-      });
-    }
-
-  } finally {
-    await prisma.$disconnect();
-  }
-};
-
-seed();
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
 
